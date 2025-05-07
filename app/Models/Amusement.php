428b1abb1e7470f1d84e72f0d6c872dc1555f0d9
Josefine <josefineahlstrand@live.se>
2025-05-07 14:51:35 +0200

    Add model for amusements

--- app/Models/Amusement.php
+++ app/Models/Amusement.php
@@ -9,4 +9,45 @@ class Amusement extends Model
 {
     /** @use HasFactory<\Database\Factories\AmusementFactory> */
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'group_id',
+        'name',
+        'type',
+        'description',
+        'image_url',
+        'url',
+    ];
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
+
+    // Relationships
+
+    /** The group that built the amusement */
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    /** All transactions on this amusement */
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    /** All stamps given from this amusement */
+    public function stamps()
+    {
+        return $this->hasMany(Stamp::class);
+    }
+
+    /** All votes cast on this amusement */
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
+
 }
