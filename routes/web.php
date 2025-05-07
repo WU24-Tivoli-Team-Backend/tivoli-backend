7b549904fa6447039fdf1d1482db2cab1bcb6f8b
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-05-07 14:02:13 +0200

    18 backend set up api contact between backend and frontend (#22)
    * Added Token functionality
    
    * Fixed CORS problems
    
    ---------
    
    Co-authored-by: Andreawingardh <andreawingardh@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::post('/tokens/create', function (Request $request) {
+
+    $token = $request->user()->createToken($request->token_name);
+
+ 
+
+    return ['token' => $token->plainTextToken];
+
 });
 
 require __DIR__.'/auth.php';
