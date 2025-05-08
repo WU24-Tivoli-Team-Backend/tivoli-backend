b567070f9bc4b64c49d2dc6934d5dd7f54fa899b
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-08 11:04:18 +0200

    Add model for groups

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -9,4 +9,16 @@ class Group extends Model
 {
     /** @use HasFactory<\Database\Factories\GroupFactory> */
     use HasFactory;
+
+    //Add this if we want to use a group name
+    // protected $fillable = [
+    //     'name',
+    // ];
+
+    //Relationships
+    /** A group has many members(users) */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
