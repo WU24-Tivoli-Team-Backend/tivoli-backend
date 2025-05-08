ff4e1332c09ecd709165cddddff795f4f100cc2f
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-08 11:24:25 +0200

    Add model for transactions

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -9,4 +9,38 @@ class Transaction extends Model
 {
     /** @use HasFactory<\Database\Factories\TransactionFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'amusement_id',
+        'group_id',
+        'user_id',
+        'stake_amount',
+        'payout_amount',
+    ];
+
+    protected $casts = [
+        'stake_amount' => 'float',
+        'payout_amount' => 'float',
+    ];
+
+
+    // Relationships
+
+    /** The amusement the transaction belongs to */
+    public function amusement()
+    {
+        return $this->belongsTo(Amusement::class);
+    }
+
+    /** The group the transaction belongs to */
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    /** The user the transaction belongs to */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
