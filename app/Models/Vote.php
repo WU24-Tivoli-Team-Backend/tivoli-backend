a11c6c3e899405d3a0821f9f4226a1596700216b
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-08 11:42:47 +0200

    Add model for votes

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -9,4 +9,23 @@ class Vote extends Model
 {
     /** @use HasFactory<\Database\Factories\VoteFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'amusement_id',
+    ];
+
+    // Relationships
+
+    /** The user who cast the vote */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /** The amusement the vote is for */
+    public function amusement()
+    {
+        return $this->belongsTo(Amusement::class);
+    }
 }
