e2f0e5a72a02e208a3eabef8348cbfd76cfda5c2
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-08 10:23:09 +0200

    Add model for stamps

--- app/Models/Stamp.php
+++ app/Models/Stamp.php
@@ -9,4 +9,25 @@ class Stamp extends Model
 {
     /** @use HasFactory<\Database\Factories\StampFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'amusement_id',
+        'animal',
+        'premium_attribute',
+    ];
+
+    //Relationships
+
+    /** A stamp belongs to a user */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /** And belongs to an amusement */
+    public function amusement()
+    {
+        return $this->belongsTo(Amusement::class);
+    }
 }
