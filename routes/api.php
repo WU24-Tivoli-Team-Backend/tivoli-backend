6d4687400bad1f321d62e603c626f6668da39eae
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-09 14:34:24 +0200

    Add routes for amusements

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\AmusementController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/test', [TestController::class, 'ping']);
+
+
+Route::apiResource('amusements', AmusementController::class);
+// Route::get    ('amusements',             [AmusementController::class, 'index']);
+// Route::post   ('amusements',             [AmusementController::class, 'store']);
+// Route::get    ('amusements/{amusement}', [AmusementController::class, 'show']);
+// Route::put    ('amusements/{amusement}', [AmusementController::class, 'update']);
+// Route::delete ('amusements/{amusement}', [AmusementController::class, 'destroy']);
