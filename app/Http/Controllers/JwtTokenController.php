da489b526bbefcb5a3ffc9bd0412e62747138fef
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-05-09 15:05:54 +0200

    implement jwt token for frontend to use

---
+++ app/Http/Controllers/JwtTokenController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Firebase\JWT\JWT;
+
+class JwtTokenController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $payload = [
+            'iss' => 'yrgobanken.vip',
+            'sub' => $user->id,
+            'email' => $user->email,
+            'iat' => time(),
+            'exp' => time() + (60 * 60), // 1 hour
+        ];
+
+        $jwt = JWT::encode($payload, env('JWT_SECRET'), 'HS256');
+
+        return response()->json(['token' => $jwt]);
+    }
+}
