6ce4ea466ecb66124e4eb9fb5fc4849962936d4d
Andreawingardh <andreawingardh@gmail.com>
2025-05-09 14:01:58 +0200

    TransactionController has been completed with CRUD-functionality
    Still needs authentication checks and conditional checks depending on whether seller is group or user. Also needs to add stamps to Transaction Model. But I'm closing this issue for now and picking up those issues in the future (issues have been created already)

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\TransactionController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/test', [TestController::class, 'ping']);
+
+/** Group API routes */
+Route::prefix('/transactions')->group(function () {
+    Route::get('/', [TransactionController::class, 'index']);
+    Route::post('/', [TransactionController::class, 'store']);
+    Route::get('/{id}', [TransactionController::class, 'show']);
+    Route::delete('/{id}', [TransactionController::class, 'destroy']);
+});
