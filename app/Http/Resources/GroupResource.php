9b557e298b86cd5db39f7ed449b0eaaf8a8ee369
JosAhl <josefineahlstrand@live.se>
2025-05-12 09:33:33 +0200

    33 api endpoint controller group (#62)
    * Add routes and display for groups
    
    * Add resource and display
    
    ---------
    
    Co-authored-by: Andrea Wingårdh <andreawingardh@gmail.com>

---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $members = $this->users->pluck('email')->toArray();
+        $members = $this->users->map(function($user) {
+            return $user->firstname . ' ' . $user->lastname;
+        })->toArray();
+
+        return [
+            'uuid' => $this->id,
+            'members' => $members,
+        ];
+    }
+}
