91c07ae7dc88050fb39a6e8c704b5497f942ecb7
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-14 10:58:44 +0200

    Feat/edit user profile (#92)
    * Add UserResource
    
    * Add index function
    
    * Add show method
    
    * Add UpdateUserRequest and add rules
    
    * Add update method to usercontroller
    
    * Add destroy method to usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,21 +1,66 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class UserController extends Controller
 {
-    public function show($id)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        try {
-            $user = User::findOrFail($id);
-            return $user;
-        } catch (ModelNotFoundException $exception) {
-            return response()->json([
-                'message' => 'User not found',
-            ], 404);
-        }
+        $user = User::all();
+        return UserResource::collection($user);
     }
 
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->update($request->validated());
+
+        return (new UserResource($user))
+            ->additional(['message' => 'User info updated successfully.']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json([
+            'message' => 'Amusement deleted',
+        ], 204);
+    }
+
+
+    // public function show($id)
+    // {
+
+    //     try {
+    //         $user = User::findOrFail($id);
+    //         return $user;
+    //     } catch (ModelNotFoundException $exception) {
+    //         return response()->json([
+    //             'message' => 'User not found',
+    //         ], 404);
+    //     }
+    // }
 }
