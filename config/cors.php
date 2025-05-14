4b45871cbed7030662d04465b17a6637d8dcf688
Andrea Wingårdh <andreawingardh@gmail.com>
2025-05-14 12:36:06 +0200

    Revert "57 api endpoint controller authentication for controllers (#100)" (#101)
    This reverts commit 148b280a4d9b55400ed2ee935c98b3df9cb1473e.

--- config/cors.php
+++ config/cors.php
@@ -25,11 +25,7 @@ return [
         env('FRONTEND_URL', 'http://localhost:3000') . '/',
         env('FRONTEND_URL', 'http://localhost:3001'),
         rtrim(env('FRONTEND_URL', 'http://localhost:3001'), '/'),
-        env('FRONTEND_URL', 'http://localhost:3001') . '/', 
-        'http://localhost:5173',
-        'http://localhost:5173/'
-
-
+        env('FRONTEND_URL', 'http://localhost:3001') . '/',
     ],
 
     'allowed_origins_patterns' => [],
