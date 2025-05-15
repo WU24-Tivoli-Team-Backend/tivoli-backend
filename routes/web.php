ceb918fdd4317f5d85027a9e0d62f34c24486908
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-05-15 11:31:36 +0200

    route for documentation in root

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,16 @@ use App\Http\Controllers\TestController;
 
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('index');
 });
 
 Route::post('/tokens/create', function (Request $request) {
 
     $token = $request->user()->createToken($request->token_name);
 
- 
+
 
     return ['token' => $token->plainTextToken];
-
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
