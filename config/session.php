acf79ee24a3e9f1b0cdf59e8649593d297c5cae5
Andreawingardh <andreawingardh@gmail.com>
2025-05-15 13:52:38 +0200

    deploy fix

--- config/session.php
+++ config/session.php
@@ -156,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', '.wu-24-tivoli.vercel.app'),
 
     /*
     |--------------------------------------------------------------------------
