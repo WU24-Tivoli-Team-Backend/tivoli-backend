4e9584b56ae4a8100b361cae00817cf30183c393
Josefine <josefineahlstrand@live.se>
2025-05-22 12:40:53 +0200

    Add user stamps for admin

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::prefix('admin')->group(function () {
     Route::post('/login', [AdminController::class, 'login'])->name('admin.login.submit');
     Route::get('/logout', [AdminController::class, 'logout'])->name('admin.logout');
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/vp', [AdminController::class, 'showUserStamps'])->name('admin.vp');
     Route::post('/reset-balances', [AdminController::class, 'resetBalances'])->name('admin.reset.balances');
     Route::post('/reset-votes', [AdminController::class, 'resetVotes'])->name('admin.reset.votes');
 });
