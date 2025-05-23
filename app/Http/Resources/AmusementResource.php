d100cd3d990176d184ca5dd85d6fa59c59c40e97
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-05-23 14:16:15 +0200

    Code clean up (#161)

--- app/Http/Resources/AmusementResource.php
+++ app/Http/Resources/AmusementResource.php
@@ -15,14 +15,14 @@ class AmusementResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id, // Not sure if we want to expose this?
-            'group_id' => $this->group_id, // Not sure if we want to expose this?
+            'id' => $this->id,
+            'group_id' => $this->group_id,
             'name' => $this->name,
             'type' => $this->type,
             'description' => $this->description,
             'image_url' => $this->image_url,
             'url' => $this->url,
-            'stamps' => $this->whenLoaded('stamps'), // Include stamps if loaded
+            'stamps' => $this->whenLoaded('stamps'),
             'stamp_id' => $this->stamp_id,
         ];
     }
